c9c2381bf0e6aa29a52ba6ab6edb73e68df4e4fb
Kevin Antonio Borge Gutierrez <antoniokevin153@outlook.com>
2026-07-25 11:37:43 -0600

    agregar abstraccion del contexto de base de dato

---
+++ PruebaTecnica.Application/Interfaces/IAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PruebaTecnica.Domain.Entities;
+
+namespace PruebaTecnica.Application.Common.Interfaces;
+
+public interface IAppDbContext
+{
+    DbSet<Category> Category { get; }
+
+    DbSet<Product> Product { get; }
+
+    DbSet<MovInv> MovInv { get; }
+
+    Task<int> SaveChangesAsync(
+        CancellationToken cancellationToken = default);
+}
