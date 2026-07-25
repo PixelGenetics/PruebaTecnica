841ca99f2ca38c25b642bb95e639d972976a9761
Kevin Antonio Borge Gutierrez <antoniokevin153@outlook.com>
2026-07-25 14:46:55 -0600

    feat: agregar eliminacion segura de productos

---
+++ PruebaTecnica.Application/Features/Products/Commands/DeleteProduct/DeleteProductResult.cs
@@ -0,0 +1,10 @@
+﻿namespace PruebaTecnica.Application.Features.Products.Commands.DeleteProduct;
+
+public class DeleteProductResult
+{
+    public bool Success { get; set; }
+
+    public bool ProductNotFound { get; set; }
+
+    public bool HasMovements { get; set; }
+}
