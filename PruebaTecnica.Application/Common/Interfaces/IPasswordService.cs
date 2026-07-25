95d908be3e08f6e2ea6029c32b8afee68c5152ee
Kevin Antonio Borge Gutierrez <antoniokevin153@outlook.com>
2026-07-25 16:47:24 -0600

    feat: agregar abstracciones para autenticacion

---
+++ PruebaTecnica.Application/Common/Interfaces/IPasswordService.cs
@@ -0,0 +1,15 @@
+﻿using PruebaTecnica.Domain.Entities;
+
+namespace PruebaTecnica.Application.Common.Interfaces;
+
+public interface IPasswordService
+{
+    string HashPassword(
+        Usuario usuario,
+        string password);
+
+    bool VerifyPassword(
+        Usuario usuario,
+        string password,
+        string passwordHash);
+}
