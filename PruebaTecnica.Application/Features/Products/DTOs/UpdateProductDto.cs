8f3cb6de3445a45bc141047059b8bf751e885b8f
Kevin Antonio Borge Gutierrez <antoniokevin153@outlook.com>
2026-07-25 14:46:13 -0600

    feat: agregar DTOs para la gestion de productos

---
+++ PruebaTecnica.Application/Features/Products/DTOs/UpdateProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EvaluacionTecnica.Models
+{
+    public class UpdateProductDto
+    {
+        //public int Id { get; set; }
+        [MaxLength(50)]
+        public required string Codigo { get; set; }
+        [MaxLength(150)]
+        public required string Nombre { get; set; }
+        //public int CategoryId { get; set; }
+        public decimal Precio { get; set; }
+        public bool Estado { get; set; } = true;
+    }
+}
