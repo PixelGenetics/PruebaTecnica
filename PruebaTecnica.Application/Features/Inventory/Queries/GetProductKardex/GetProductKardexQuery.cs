b3fe3f227cc263a860695bac6b239fefbe1f30a9
Kevin Antonio Borge Gutierrez <antoniokevin153@outlook.com>
2026-07-25 15:36:20 -0600

    feat: agregar historial kardex con saldo acumulado

---
+++ PruebaTecnica.Application/Features/Inventory/Queries/GetProductKardex/GetProductKardexQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using PruebaTecnica.Application.Features.Inventory.DTOs;
+
+namespace PruebaTecnica.Application.Features.Inventory.Queries.GetProductKardex;
+
+public record GetProductKardexQuery(int ProductId)
+    : IRequest<ProductKardexDto?>;
